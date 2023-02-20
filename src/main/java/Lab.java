0133e7b2f90965ae20de89ceeb64b9fe64caf9a2
Cameron Robinson <cameron.robinson804@gmail.com>
2023-02-20 22:04:57 +0000

    LabDone

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,12 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
+    public void throwUncheckedException()
+    { int x = 0;
+        int y = 32;
+        int z = y/x;
+
+        
 
     }
 }
